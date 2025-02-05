5f349aeadd3f9c01467190ca5c1d05bc485e933c
JFAquinoPando <pandolcik@gmail.com>
2025-02-05 13:15:05 -0300

    optimizar muestra de imagen

--- ecommerce/imagen.php
+++ ecommerce/imagen.php
@@ -1,7 +1,18 @@
 <?php
 header("Content-Type: image/webp");
 $archivo = $_GET["nombre"];
-$i = imagecreatefromwebp(__DIR__."/../carga-productos/imagen-productos/{$archivo}");
+/* $i = imagecreatefromwebp(__DIR__."/../carga-productos/imagen-productos/{$archivo}");
 imagewebp($i);
-imagedestroy($i);
+imagedestroy($i); */
 
+$ruta_archivo = __DIR__."/../carga-productos/imagen-productos/{$archivo}";
+
+// Verificar si el archivo existe y es legible
+if (is_readable($ruta_archivo)) {
+  // Leer el archivo y enviarlo directamente al navegador
+  readfile($ruta_archivo);
+} else {
+  // Manejar el caso en que el archivo no existe o no se puede leer
+  http_response_code(404);
+  echo "Imagen no encontrada.";
+}
