b26cf985ef7faa5bed9d0023a381ce2aa892f04c
JFAquinoPando <pandolcik@gmail.com>
2025-03-29 16:36:52 -0300

    descargar imagen

---
+++ ecommerce/descargar.php
@@ -0,0 +1,17 @@
+<?php
+// descargar.php
+$img = "https://woocommercecore.mystagingwebsite.com/wp-content/uploads/2017/12/vneck-tee-2.jpg";
+$nombre =basename($img);
+$descarga = file_get_contents($img);
+
+file_put_contents(__DIR__."/{$nombre}", $descarga);
+//echo "ARCHIVO DESCARGADO";
+
+function descargarImg($url){
+    $nombre =basename($url);
+    $descarga = file_get_contents($url);
+    file_put_contents(__DIR__."/{$nombre}", $descarga);
+    return $nombre;
+    
+}
+
