5bc397021d1d7c95bd85865802bdd9ebf254f5c3
JFAquinoPando <pandolcik@gmail.com>
2025-03-29 17:12:46 -0300

    descargar imagen

--- ecommerce/descargar.php
+++ ecommerce/descargar.php
@@ -4,8 +4,8 @@ $img = "https://woocommercecore.mystagingwebsite.com/wp-content/uploads/2017/12/
 $nombre =basename($img);
 $descarga = file_get_contents($img);
 
-file_put_contents(__DIR__."/{$nombre}", $descarga);
-//echo "ARCHIVO DESCARGADO";
+file_put_contents(__DIR__."/../carga-productos/imagen-productos/{$nombre}", $descarga);
+echo "ARCHIVO DESCARGADO";
 
 function descargarImg($url){
     $nombre =basename($url);
